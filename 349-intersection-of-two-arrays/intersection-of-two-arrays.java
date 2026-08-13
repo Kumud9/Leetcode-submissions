ee1e55fe507102a10448ef761ead58fbeb5398fb
Kumud9 <wckumud9@gmail.com>
2026-08-13 10:08:05 +0530

    Time: 2 ms (97.18%) | Memory: 45.1 MB (40.54%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set= new HashSet<>();
+         HashSet<Integer> ans= new HashSet<>();
+
+         for(int x: nums1){
+            set.add(x);
+         }
+         for(int x: nums2){
+            if(set.contains(x))ans.add(x);
+         }
+         int [] arr= new int[ans.size()];
+         int i=0;
+         for(int a: ans){
+            arr[i++]=a;
+         }
+
+         return arr;
+    }
+}
