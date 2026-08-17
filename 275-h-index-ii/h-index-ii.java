b8c3ddd131960461d8540f5359b7fd964eb03251
Kumud9 <wckumud9@gmail.com>
2026-08-18 00:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (11.45%) - LeetSync

--- 275-h-index-ii/h-index-ii.java
+++ 275-h-index-ii/h-index-ii.java
@@ -1,13 +1,25 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int h=0;
-       for(int i=citations.length-1;i>=0;i--){
-        if(citations[i]>h){
-            h++;
-        }else{
-            break;
+    //     int h=0;
+    //    for(int i=citations.length-1;i>=0;i--){
+    //     if(citations[i]>h){
+    //         h++;
+    //     }else{
+    //         break;
+    //     }
+    //    }   
+    //    return h;
+    int l= 0;int r = citations.length - 1;
+        while (l<=r) {
+            int mid = l+(r-l) / 2;
+            if (citations[mid] == citations.length- mid) {
+                return citations[mid];
+            } else if (citations[mid] < citations.length - mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
-       }   
-       return h;
+        return citations.length - l;
     }
 }
