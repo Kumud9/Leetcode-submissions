a23cc6a6981411c6f4554a4bdb6971553ccdba59
Kumud9 <wckumud9@gmail.com>
2026-08-24 08:57:37 +0530

    Time: 32 ms (29.44%) | Memory: 46.8 MB (72.01%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,26 +1,37 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        HashMap<Character,Integer> smap= new HashMap<>();
-        HashMap<Character,Integer> pmap= new HashMap<>();
+        HashMap<Character,Integer> map= new HashMap<>();
         List<Integer> res= new ArrayList<>();
         if(s.length()<p.length())return res;
 
         for(int i=0;i<p.length();i++){
-            pmap.put(p.charAt(i),pmap.getOrDefault(p.charAt(i),0)+1);
-            smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
+            map.put(p.charAt(i),map.getOrDefault(p.charAt(i),0)+1);
+           
         }
-        if(smap.equals(pmap))res.add(0);
+    
+
          int l=0;
-         int r= p.length();
+         int r= 0;
+         int cnt=p.length();
          while(r<s.length()){
-            char aq= s.charAt(r++);
-            smap.put(aq,smap.getOrDefault(aq,0)+1);
+            char ch= s.charAt(r);
+            if(map.containsKey(ch)){
+                if(map.get(ch)>0)cnt--;
+                map.put(ch,map.get(ch)-1);
+            }
+            r++;
 
-            char dis =s.charAt(l++);
-            smap.put(dis,smap.get(dis)-1);
-            if(smap.get(dis)==0)smap.remove(dis);
+            if(r-l>p.length()){
+                 char left= s.charAt(l);
+            if(map.containsKey(left)){
+                map.put(left,map.get(left)+1);
 
-             if(smap.equals(pmap))res.add(l);
+                 if(map.get(left)>0)cnt++;
+            }
+            l++;
+            }
+
+           if (cnt==0)res.add(l);
          }
         
         return res;
