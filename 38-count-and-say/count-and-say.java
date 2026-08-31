eaa8f68d63dbd60dce274a66036c20ef7971aab2
Kumud9 <wckumud9@gmail.com>
2026-08-31 22:35:41 +0530

    Time: 10 ms (20.08%) | Memory: 42.7 MB (96.53%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String countAndSay(int n) {
+       String s="1";
+       for(int i=1;i<n;i++){
+        StringBuilder sb=new StringBuilder();
+        int cnt=1;
+        for(int j=1;j<=s.length();j++){
+           if(j<s.length() && s.charAt(j)==s.charAt(j-1)){
+            cnt++;
+           }
+           else {
+            sb.append(cnt);
+            sb.append(s.charAt(j-1));
+            cnt=1;
+           }
+        }
+        s=sb.toString();
+       }
+       return s;
+    }
+}
